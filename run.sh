4a669fbae7ff4cb2cc949e2080442c9dbf5d008f
Linda Goldstein <linda.goldstein@sonicdrivein.com>
2016-10-14 13:00:23 -0700

    adding warning to runfile

--- run.sh
+++ run.sh
@@ -7,8 +7,9 @@ echo "gemset:"
 rvm gemset list
 
 # this gem was built by me from the source https://github.com/compwron/wikipedia-vandalism-detection/commit/1a540566b9a115eb697a014d03f1f49fb30d9da9
-rm -rf vendor/gems/*
-gem unpack wikipedia-vandalism_detection-0.0.1.gem --target vendor/gems/
+# rm -rf vendor/gems/*
+# gem unpack wikipedia-vandalism_detection-0.0.1.gem --target vendor/gems/
+echo "WARNING: using manually built and modified gemfile in vendor/gems/wikipedia-vandalism_detection-0.0.1"
 
 echo "bundle install"
 bundle install
